f4e7aa4bdecad31f275d882bf705e23b660ff468
kurtaszone <muhammadsalman504@gmail.com>
2026-04-25 12:48:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -88,3 +88,8 @@ def pay():
 if __name__ == "__main__":
     init_db()
     app.run()
+    import os
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
