e7d9279440ee7eb14523476a625ba985eead809d
kurtaszone <muhammadsalman504@gmail.com>
2026-04-25 12:49:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,8 @@ if __name__ == "__main__":
     app.run()
     import os
 
-if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+import os
+
+init_db()
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
